c984d14ddfdaf7dea0e3f692ced2ffcdeb0244b8
Jigar Mav <41746411+JigarMav@users.noreply.github.com>
2020-09-03 14:56:29 +0530

    Update 299_Bulls_and_Cows.cpp

--- C Plus Plus/Strings/299_Bulls_and_Cows.cpp
+++ C Plus Plus/Strings/299_Bulls_and_Cows.cpp
@@ -4,9 +4,8 @@ public:
 	//  APPROACH
         // if guess is at same loc and val its bull 
         // if guess is at diff loc then store count and then compute the min as cow
-        //     else \
-        //         nuting
 
+	    
         // Two arrays approach (Two pass) .
         int bull=0,cow = 0;
         vector<int> sec_cnt(10,0);
@@ -27,30 +26,38 @@ public:
             }
            
         }
+// 	    count the matching numbers only.
         for(int i=0;i<10;i++)
             cow+=min(sec_cnt[i],guess_cnt[i]);
 
 
 	/*
+	
 			//IDEAL SOLUTION	
-		//One pass solution using 1 array.
-		   vector<int>vi(10);
-        	int bull=0,cows=0;
-       	 	 for(int i=0;i<sz;i++) {
-           			 if(secret[i]==guess[i])
-               		 bull++;
-            else {
-                if(vi[secret[i]-'0']++<0)
-                    cows++;
-                if(vi[guess[i]-'0']-->0)
-                    cows++;
-            }
-        }
+	//One pass solution using 1 array.
+// 	vi[secret[i]-'0'] is negative only if this character appeared in the guess more times then in the secret
+// 	which means that this character in the secret can be matched with one of the previous
+// 	characters in the guest.	
+	
+	vector<int>vi(10);
+	int bull=0,cows=0;
+	for(int i=0;i<sz;i++) {
+	   if(secret[i]==guess[i])
+		 bull++;
+	   else {
+		if(vi[secret[i]-'0']++<0)
+			cows++;
+		if(vi[guess[i]-'0']-->0)
+			cows++;
+	    }
+	}
 		
 	*/
-        // cout<<bull<<cow<<endl;
+	    
         return to_string(bull) + "A" + to_string(cow) + "B";
-        
+	    
+//         Space - O(N)
+// 	    Time - O(N).
         
     }
 };
